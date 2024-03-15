13171f1eab1f5cafb3315a3c04f56654839d7211
Fabiodsnm <fabiodsmatos.is@gmail.com>
2024-03-15 13:41:15 +0000

    splash screen

--- app/src/main/java/pt/fabiomatos/connectedcinema/ui/theme/Color.kt
+++ app/src/main/java/pt/fabiomatos/connectedcinema/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package pt.fabiomatos.connectedcinema.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+object ConnectedCinemaColor {
+    val Black = Color(0xFF171A1F)
+    val White = Color(0xFFFFFFFF)
+    val Yellow = Color(0xFFEFB034)
+    val Red = Color(0xFFDE3B40)
+    val White35 = Color(0x59FFFFFF)
+}
--- app/src/main/java/pt/fabiomatos/connectedcinema/ui/theme/Theme.kt
+++ app/src/main/java/pt/fabiomatos/connectedcinema/ui/theme/Theme.kt
@@ -16,15 +16,16 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = ConnectedCinemaColor.Black,
+    secondary = ConnectedCinemaColor.White,
+    tertiary = ConnectedCinemaColor.Yellow
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = ConnectedCinemaColor.Black,
+    secondary = ConnectedCinemaColor.White,
+    tertiary = ConnectedCinemaColor.Yellow,
+    background = ConnectedCinemaColor.Black,
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -41,7 +42,7 @@ private val LightColorScheme = lightColorScheme(
 fun ConnectedCinemaTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
