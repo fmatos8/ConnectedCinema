8f7d09ddeae957b4603ad819391f430dad7e95aa
Fabiodsnm <fabiodsmatos.is@gmail.com>
2024-04-01 17:16:49 +0100

    login (cont)

---
+++ app/src/main/java/pt/fabiomatos/connectedcinema/data/DataSource.kt
@@ -0,0 +1,11 @@
+package pt.fabiomatos.connectedcinema.data
+
+import pt.fabiomatos.connectedcinema.R
+
+object DataSource {
+    val loginOptions = listOf(
+        Pair("Google", R.drawable.button_1),
+        Pair("Facebook", R.drawable.button_2),
+        Pair("Apple", R.drawable.button_3)
+    )
+}
--- app/src/main/java/pt/fabiomatos/connectedcinema/ui/theme/Type.kt
+++ app/src/main/java/pt/fabiomatos/connectedcinema/ui/theme/Type.kt
@@ -2,33 +2,46 @@ package pt.fabiomatos.connectedcinema.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import pt.fabiomatos.connectedcinema.R
+
+private val myCustomFont = FontFamily(
+    Font(R.font.robotocondensed_regular),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.robotocondensed_regular, FontWeight.Normal),
+    Font(R.font.robotocondensed_light,FontWeight.Light),
+    Font(R.font.robotocondensed_bold, FontWeight.Bold),
+    Font(R.font.robotocondensed_thin, FontWeight.Thin)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = RobotoCondensed,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    displayLarge= TextStyle(fontFamily = RobotoCondensed),
+    displayMedium= TextStyle(fontFamily = RobotoCondensed),
+    displaySmall= TextStyle(fontFamily = RobotoCondensed),
+    headlineLarge= TextStyle(fontFamily = RobotoCondensed),
+    headlineMedium= TextStyle(fontFamily = RobotoCondensed),
+    headlineSmall= TextStyle(fontFamily = RobotoCondensed),
+    titleLarge= TextStyle(fontFamily = RobotoCondensed),
+    titleMedium= TextStyle(fontFamily = RobotoCondensed),
+    titleSmall= TextStyle(fontFamily = RobotoCondensed),
+    bodyMedium= TextStyle(fontFamily = RobotoCondensed),
+    bodySmall= TextStyle(fontFamily = RobotoCondensed),
+    labelLarge= TextStyle(fontFamily = RobotoCondensed),
+    labelMedium= TextStyle(fontFamily = RobotoCondensed),
+    labelSmall= TextStyle(fontFamily = RobotoCondensed),
+
 )
