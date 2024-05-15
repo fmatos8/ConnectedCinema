8fa9d5d5ae2269a41b2e23828163572b0c3fac8b
Fabiodsnm <fabiodsmatos.is@gmail.com>
2024-05-15 18:03:53 +0100

    testes

--- app/src/main/java/pt/fabiomatos/connectedcinema/ui/views/screens/HomeScreen.kt
+++ app/src/main/java/pt/fabiomatos/connectedcinema/ui/views/screens/HomeScreen.kt
@@ -1,37 +1,134 @@
 package pt.fabiomatos.connectedcinema.ui.views.screens
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.R
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.filled.ArrowForwardIos
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import kotlinx.coroutines.launch
+import pt.fabiomatos.connectedcinema.ui.navigation.Login
+import pt.fabiomatos.connectedcinema.ui.navigation.WelcomeItem
+import pt.fabiomatos.connectedcinema.ui.theme.ConnectedCinemaTheme
+import pt.fabiomatos.connectedcinema.ui.views.ImageSlider
+import pt.fabiomatos.connectedcinema.utils.Utils
 
 @Composable
 fun HomeScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .wrapContentSize(Alignment.Center)
-    ) {
-        Text(
-            text = "HomeScreen",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = Modifier.align(Alignment.CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 25.sp
+    val images = Utils.welcomeImages
+    ConnectedCinemaTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            ImageSlider(images = images)
+        }
+    }
+}
+
+@Composable
+fun ImageItem(imageRes: WelcomeItem){
+    Column {
+        Image(
+            painter = painterResource(id = imageRes.icon),
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxWidth()
+                .size(350.dp)
         )
     }
+}
+
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun ImageSlider(images: List<WelcomeItem>) {
+
+    val pageCount = images.size
+    val pagerState = rememberPagerState(pageCount = { pageCount })
+
+    BoxWithConstraints(modifier = Modifier.fillMaxWidth(1f)) {
+
+    }
+
+
+    HorizontalPager(
+        state = pagerState
+    ) { page ->
+        ImageItem(imageRes = images[page])
+    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 60.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        repeat(pageCount) { iteration ->
+            val color =
+                if (pagerState.currentPage == iteration) Color.Red
+                else MaterialTheme.colorScheme.primary
+            Box(
+                modifier = Modifier
+                    .padding(5.dp)
+                    .clip(CircleShape)
+                    .background(color)
+                    .size(10.dp)
+            ) {
+
+            }
+        }
+    }
+
+
+
 }
 
 @Preview(showBackground = true)
